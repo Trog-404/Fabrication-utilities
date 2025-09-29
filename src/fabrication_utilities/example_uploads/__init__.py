71b90b8383038cd917bd45a86321de2428759b5f
Trog-404 <bontornomatteo@gmail.com>
2025-09-29 12:47:29 +0200

    Added example upload and new version out

--- src/fabrication_utilities/example_uploads/__init__.py
+++ src/fabrication_utilities/example_uploads/__init__.py
@@ -1,11 +1,11 @@
 from nomad.config.models.plugins import ExampleUploadEntryPoint
 
 process_upload_entry_point = ExampleUploadEntryPoint(
-    title='Fabrication process and steps',
-    category='Fabrication examples',
+    title='Fabrication process',
+    category='ELNs for fabrication',
     description="""
         This example upload show the method of storing raw data in nomad for fabrication
-        processes. This should be help a FAIR pipeline for data stored in Nomad.
+        processes. This should be help a FAIR pipeline for data stored in NOMAD.
     """,
     resources=['processes/*'],
 )
